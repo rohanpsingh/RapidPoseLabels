47521832c5fc7b16c57c5dff3460030e1b37f028
rohanpsingh <rohan565singh@gmail.com>
2020-06-02 17:16:37 +0900

    code refactoring: visualize3d.py

--- utility/visualize3d.py
+++ utility/visualize3d.py
@@ -1,105 +1,10 @@
 import numpy as np
 import argparse
 import open3d as o3d
-import transforms3d.quaternions as tfq
 import transforms3d.affines as tfa
 import os, sys
 from skimage.util.shape import view_as_blocks
-
-np.set_printoptions(threshold=sys.maxsize, linewidth=700)
-np.set_printoptions(precision=4, suppress=True)
-
-def visualize_keypoints(X, Y, T, model_off_path):
-    vis_mesh_list = []
-    vis_mesh_list.append(o3d.geometry.TriangleMesh.create_coordinate_frame(0.2))
-
-    if model_off_path is not None:
-        obj_mesh = o3d.io.read_triangle_mesh(model_off_path)
-        obj_mesh.paint_uniform_color([0.6, 0.6, 0.1])
-        obj_cloud = obj_mesh.sample_points_uniformly(5000)
-        vis_mesh_list.append(obj_cloud)
-
-    for pt in X:
-        keypt_mesh = o3d.geometry.TriangleMesh.create_sphere(radius=0.006)
-        pt = np.dot(T[:3,:3], pt) + T[:3,3]
-        keypt_mesh.translate(pt)
-        keypt_mesh.paint_uniform_color([0.1, 0.1, 0.7]) #blue
-        vis_mesh_list.append(keypt_mesh)
-
-    for pt in Y:
-        keypt_mesh = o3d.geometry.TriangleMesh.create_sphere(radius=0.005)
-        keypt_mesh.translate(pt)
-        keypt_mesh.paint_uniform_color([0.7, 0.1, 0.1]) #red
-        vis_mesh_list.append(keypt_mesh)
-
-    o3d.visualization.draw_geometries(vis_mesh_list)
-
-def procrustes(X, Y, scaling=True, reflection='best'):
-    n,m = X.shape
-    ny,my = Y.shape
-    muX = X.mean(0)
-    muY = Y.mean(0)
-    X0 = X - muX
-    Y0 = Y - muY
-    ssX = (X0**2.).sum()
-    ssY = (Y0**2.).sum()
-
-    normX = np.sqrt(ssX)
-    normY = np.sqrt(ssY)
-    X0 /= normX
-    Y0 /= normY
-    if my < m:
-        Y0 = np.concatenate((Y0, np.zeros(n, m-my)),0)
-
-    A = np.dot(X0.T, Y0)
-    U,s,Vt = np.linalg.svd(A,full_matrices=False)
-    V = Vt.T
-    T = np.dot(V, U.T)
-
-    if reflection is not 'best':
-        have_reflection = np.linalg.det(T) < 0
-        if reflection != have_reflection:
-            V[:,-1] *= -1
-            s[-1] *= -1
-            T = np.dot(V, U.T)
-    traceTA = s.sum()
-    if scaling:
-        b = traceTA * normX / normY
-        d = 1 - traceTA**2
-        Z = normX*traceTA*np.dot(Y0, T) + muX
-    else:
-        b = 1
-        d = 1 + ssY/ssX - 2 * traceTA * normY / normX
-        Z = normY*np.dot(Y0, T) + muX
-    if my < m:
-        T = T[:my,:]
-    c = muX - b*np.dot(muY, T)
-    tform = {'rotation':T, 'scale':b, 'translation':c}
-    return d, Z, tform
-
-def get_visibility(select):
-    vec = []
-    row, col = 0, 0
-    while col<(select.shape[1]):
-        m = select[row:row+3, col:col+3]
-        if (m.shape[0]==0) or not (m == np.eye(3)).all():
-            vec.append(0)
-        else:
-            vec.append(1)
-            row+=3
-        col+=3
-    return vec
-
-def get_object_definition(pp_file, vec):
-    with open(pp_file, 'r') as file:
-        lines = [[float(i.rsplit('=')[1].rsplit('"')[1]) for i in line.split()[1:4]] for line in file.readlines()[8:-1]]
-    out = [kpt for v, kpt in zip(vec, lines) if v]
-    return np.asarray(out)
-        
-def get_object_manual(kpts, vec):
-    out = [kpt for v, kpt in zip(vec, kpts.transpose()) if v]
-    return np.asarray(out)
-
+import evaluate3d
 
 def process(input_array, pp, off):
     ref_kpts   = input_array['ref']
@@ -111,14 +16,14 @@ def process(input_array, pp, off):
     print("Number of keypts: ", num_keypts)
 
     sce_id  = 0
-    vis_vec = get_visibility(select_mat[:3*num_keypts,:3*num_keypts])
-    obj_man = get_object_manual(ref_kpts[sce_id], vis_vec)
-    obj_def = get_object_definition(pp, vis_vec)
-    d, Z, tform = procrustes(obj_def, obj_man, False)
+    vis_vec = evaluate3d.get_visibility(select_mat[:3*num_keypts,:3*num_keypts])
+    obj_man = evaluate3d.get_object_manual(ref_kpts[sce_id], vis_vec)
+    obj_def = evaluate3d.get_object_definition(pp, vis_vec)
+    d, Z, tform = evaluate3d.procrustes(obj_def, obj_man, False)
 
     T = tfa.compose(tform['translation'], np.linalg.inv(tform['rotation']), np.ones(3))
     
-    obj_all = get_object_definition(pp, np.ones(num_keypts))
+    obj_all = evaluate3d.get_object_definition(pp, np.ones(num_keypts))
     obj_est = opt_output[(num_scenes-1)*7:].reshape((num_keypts, 3))
     err = []
     for (x, y) in zip(obj_all, obj_est):
@@ -126,9 +31,9 @@ def process(input_array, pp, off):
         err.append(((x-y)**2).sum()**0.5)
 
     print("Mean error: ", sum(err)/len(err))
-    print("---")
     if visualize:
-        visualize_keypoints(obj_est, obj_all, T, off)
+        evaluate3d.visualize_keypoints(obj_est, obj_all, T, off, None)
+    print("---")
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
@@ -153,6 +58,3 @@ if __name__ == '__main__':
         process(input_array, pp_file_path, off_file_path)
 
     sys.exit(-1)
-
-
-
