85094a53475aa23f8449de790a9d6240625fe03b
rohanpsingh <rohan565singh@gmail.com>
2020-07-21 15:58:49 +0900

    2d evaluation script code refactoring

---
+++ utility/procrustes.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+def procrustes(X, Y, scaling=True, reflection='best'):
+    n,m = X.shape
+    ny,my = Y.shape
+    muX = X.mean(0)
+    muY = Y.mean(0)
+    X0 = X - muX
+    Y0 = Y - muY
+    ssX = (X0**2.).sum()
+    ssY = (Y0**2.).sum()
+
+    normX = np.sqrt(ssX)
+    normY = np.sqrt(ssY)
+    X0 /= normX
+    Y0 /= normY
+    if my < m:
+        Y0 = np.concatenate((Y0, np.zeros(n, m-my)),0)
+
+    A = np.dot(X0.T, Y0)
+    U,s,Vt = np.linalg.svd(A,full_matrices=False)
+    V = Vt.T
+    T = np.dot(V, U.T)
+
+    if reflection is not 'best':
+        have_reflection = np.linalg.det(T) < 0
+        if reflection != have_reflection:
+            V[:,-1] *= -1
+            s[-1] *= -1
+            T = np.dot(V, U.T)
+    traceTA = s.sum()
+    if scaling:
+        b = traceTA * normX / normY
+        d = 1 - traceTA**2
+        Z = normX*traceTA*np.dot(Y0, T) + muX
+    else:
+        b = 1
+        d = 1 + ssY/ssX - 2 * traceTA * normY / normX
+        Z = normY*np.dot(Y0, T) + muX
+    if my < m:
+        T = T[:my,:]
+    c = muX - b*np.dot(muY, T)
+    tform = {'rotation':T, 'scale':b, 'translation':c}
+    return d, Z, tform
